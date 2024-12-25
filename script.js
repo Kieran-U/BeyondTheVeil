4c0ec1b21474cd84803f5bf8cd2ff8f0dc1f2ca1
Kieran-U <kieran.lightray@gmail.com>
2024-12-25 15:04:06 -0500

    LocationSlab

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map', {zoomControl: false}).setView([51.505, -0.09], 13);
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 3,
+    maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var watercolor = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
+	minZoom: 1,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
 });
 
 osm.addTo(map);
@@ -23,14 +32,14 @@ fetch("Markers.json")
                     });
 
                     // Color tag icons
-                    if (markerData.tags.color == "red") {
+                    if (markerData.tags.series == "Percy Jackson") {
                         icon = L.icon({
                             iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/refs/heads/master/img/marker-icon-2x-red.png',  // Blue marker
                             iconSize: [25, 41],
                             iconAnchor: [12, 41],
                             popupAnchor: [1, -34]
                         });
-                    } else if (markerData.tags.color == "green") {
+                    } else if (markerData.tags.series == "Harry Potter") {
                         icon = L.icon({
                             iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/refs/heads/master/img/marker-icon-2x-green.png',  // Green marker
                             iconSize: [25, 41],
@@ -40,7 +49,43 @@ fetch("Markers.json")
                     }
                     
                     const marker = L.marker([lat, lon], { icon: icon }).addTo(map);
-                    marker.bindPopup(`<b>${markerData.Name}</b><br>${markerData.description}`);
+
+                    //Label for marker
+                    marker.bindTooltip(markerData.Name, {
+                        permanent: true,  // Keep the label on the map even when not hovered
+                        direction: "top",  // Position the label above the marker
+                        offset: L.point(0, -35),  // Adjust the label's position (10 pixels above the marker)
+                        className: "NB-Tool-Tip" // For css style No-Box
+                      }).openTooltip();
+
+                      //Event Listener Click on Marker
+                      marker.on('click', function() {
+                        updateLocationSlab(markerData); // Update the left menu with the marker's info
+                      });
                 });
             })
             .catch(error => console.error('Error loading the JSON file:', error));
+
+map.on('click', function() {
+    hideLocationSlab(); // Hide the menu if user clicks somewhere on the map
+});
+
+function updateLocationSlab(marker) {
+    var LS = document.getElementById('LocationSlab');
+    LS.innerHTML = `
+        <div class="LS-img">${marker.images.map(image => `<img src="${image}" alt="Image">`).join('')}</div>
+        <h2>${marker.Name}</h2>
+        <div class="tags">${Object.values(marker.tags).map(tagValue => {
+            // Replace spaces with underscores in the tag value
+            const tagClass = tagValue.replace(/\s+/g, '_'); 
+            return tagValue ? `<span class="tag ${tagClass}">${tagValue}</span>` : '';
+        }).join('')}</div>
+        <p>${marker.description}</p>
+    `;
+    LS.style.display = 'block';
+}
+
+function hideLocationSlab() {
+    var menu = document.getElementById('LocationSlab');
+    menu.style.display = 'none'; // Hide the menu when no marker is clicked
+}
